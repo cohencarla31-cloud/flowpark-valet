4c6f5a1de7ad952e240a227b8c12c5e30706e566
cohencarla31-cloud <cohencarla31@gmail.com>
2026-08-13 19:48:09 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,21 @@
 import streamlit as st
-import pandas as pd
 import gspread
-from oauth2client.service_account import ServiceAccountCredentials
+import json
+
+# Configuración de la conexión usando los secretos
+creds_dict = st.secrets["gcp_service_account"]
+client = gspread.service_account_from_dict(creds_dict)
+
+# AQUÍ PEGUE EL LINK DE TU GOOGLE SHEET (el que termina en /edit)
+SHEET_URL = "https://docs.google.com/spreadsheets/d/1jknI7amSqutxGT_WAIBNhD0vezFE2AueTEzP7q5Rb1c/edit?gid=0#gid=0"
+sh = client.open_by_url(SHEET_URL)
+worksheet = sh.sheet1 # Esto conecta con la primera pestaña
 
 st.title("🚗 FlowPark - Valet Parking")
-st.write("Control de ingresos y salidas en puerta.")
 
-# Formulario simple para el valet
-patente = st.text_input("Matrícula / Patente del Vehículo:")
-propietario = st.text_input("Nombre del Propietario / Invitado:")
-accion = st.radio("Acción:", ["Ingresa a Playa", "Se Retira (Aviso Mesa)"])
+patente = st.text_input("Matrícula:")
+propietario = st.text_input("Propietario:")
 
-if st.button("Registrar Operación"):
-    if patente:
-        st.success(f"¡Vehículo {patente} registrado con éxito!")
-    else:
-        st.warning("Por favor, ingresa al menos la patente.")
+if st.button("Registrar"):
+    worksheet.append_row([patente, propietario]) # Esto escribe en el Excel
+    st.success("Guardado en la base de datos.")
