6f590d7c54ce1032a000dbacd576d2b02db8cf8d
cohencarla31-cloud <cohencarla31@gmail.com>
2026-08-13 21:39:32 -0300

    Update app.py

--- app.py
+++ app.py
@@ -12,11 +12,10 @@ def init_connections():
     creds_dict = st.secrets["gcp_service_account"]
     client = gspread.service_account_from_dict(creds_dict)
     
-    # 1. Tu Planilla Maestra de Valet (REEMPLAZA TU LINK AQUÍ)
-    SHEET_VALET_URL = "https://docs.google.com/spreadsheets/d/1jknI7amSqutxGT_WAIBNhD0vezFE2AueTEzP7q5Rb1c/edit"
-    sh_valet = client.open_by_url(SHEET_VALET_URL)
+    # 1. Tu Planilla Maestra de Valet abierta por su nombre exacto en Drive
+    sh_valet = client.open("FlowPark_Valet_DB")
     
-    # 2. Planilla del Formulario de Quinquela (Conectada directo por código)
+    # 2. Planilla del Formulario de Quinquela
     id_quinquela = "18ufUYyHmDbqAb74Cu2mS7i6L6JBRJZQyxoR10GBOwaM"
     sh_quinquela = client.open_by_key(id_quinquela)
     
